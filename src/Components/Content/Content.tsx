236da6f6372cb8ea4adaf500b59263498c3ef0a8
OhmxSarawuth <sarawuthza01@gmail.com>
2025-03-28 14:22:49 +0700

    add some new code

--- src/Components/Content/Content.tsx
+++ src/Components/Content/Content.tsx
@@ -1,7 +1,10 @@
 export function Content(){
+    const hello = ()=>{
+        console.log("my function");
+    }
     return (
         <div>
-            
+            <p>this is my first change commit git.</p>
         </div>
     )
 }
